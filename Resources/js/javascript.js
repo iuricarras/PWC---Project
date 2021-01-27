6fc07cb7bfb0cf4cf00cd7ad7140d934b71c1746
iuricarras <iuri.carrasqueiro@gmail.com>
2021-01-27 11:05:46 +0000

    Home - 1ª Versão Carousel Top 10

--- Resources/js/javascript.js
+++ Resources/js/javascript.js
@@ -66,3 +66,33 @@ $(function () {
 	});
 }
 });	
+ 
+ $( document ).ready(function() {
+    $.ajax({
+            type : 'POST',
+            url : 'http://ws.audioscrobbler.com/2.0/',
+            data : 'method=geo.gettoptracks&' +
+                    'country=portugal&' +
+                    'api_key=57ee3318536b23ee81d6b27e36997cde&' +
+                    'format=json',
+            dataType : 'jsonp',
+            success : function(data) {
+             for (var i = 0; i <= 10; i++) {
+                    
+                    var liMedia;
+                    var link = data.tracks.track[0].image[0]["#text"];
+
+                    console.log(link);
+
+                    $('#musicimage', liMedia).attr("src", link);
+                    $('.title' + i, liMedia).html(data.tracks.track[i].name);
+                    $('.artist' + i, liMedia).html(data.tracks.track[i].artist.name);
+                    $('.music'+ i).append(liMedia);
+
+                    console.log('data');
+                    console.log(data);
+            }
+        }
+    });
+
+})
